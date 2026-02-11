6fc8bd35e2e920ba4e4558fac2d8323d4c1aa80e
Vasily Kiselev <vasili.kiselev@gmail.com>
2026-02-11 16:49:00 +0300

    Next Migration step

--- new/src/content/config.ts
+++ new/src/content/config.ts
@@ -1,18 +1,19 @@
 import { defineCollection, z } from 'astro:content';
 
 const baseSchema = ({ image }: { image: any }) => z.object({
-  hover_text: z.string(),
-  url: z.string().optional(),
-  youtube: z.string().optional(),
-  width: z.number().optional(),
-  height: z.number().optional(),
-  pages: z.number().optional(),
-  twitter: z.string().optional(),
-  facebook: z.string().optional(),
-  the_book: z.string().optional(),
+  hover_text: z.union([z.string(), z.number()]).nullable().optional(),
+  url: z.string().nullable().optional(),
+  youtube: z.string().nullable().optional(),
+  width: z.number().nullable().optional(),
+  height: z.number().nullable().optional(),
+  pages: z.number().nullable().optional(),
+  twitter: z.string().nullable().optional(),
+  facebook: z.string().nullable().optional(),
+  the_book: z.string().nullable().optional(),
   thumbnail: image().optional(),
   cover: image().optional(),
   full: image().optional(),
+  pages_images: z.array(image()).optional(),
 });
 
 export const collections = {
